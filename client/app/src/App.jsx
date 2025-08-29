dd2b3cf84a1ec543d6668e8b5c0922380133de1f
prakritii-i <acharyaprakriti621@gmail.com>
2025-08-29 23:57:46 +0545

    Backed adn Frontend connected via GET method

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,19 +1,55 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState([]);
+  const [title,setTitle] = useState("");
+  const [release_year, setRelease_year]= useState(0);
+
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
+      const data = await response.json();
+     // console.log(data);
+      setBooks(data);  // Update state with fetched data
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
       <h1>Books Website</h1>
       <div>
-        <input type="text" name="" id="" placeholder='Book Title....'/>
-        <input type="text" name="" id="" placeholder='Release Date'/>
+        <input type="text" name="" id="" placeholder="Book Title...." />
+        <input type="text" name="" id="" placeholder="Release Year" />
         <button>Add Book</button>
       </div>
+      {books.map((book) => 
+      <div>
+        <p>Title: {book.title}</p>
+        <p>Release year: {book.release_year} </p>
+      </div>
+      )}
+      {/* <div>
+        <h2>Books List</h2>
+        {books.length > 0 ? (
+          <ul>
+            {books.map((book, index) => (
+              <li key={index}>{book.title} ({book.release_date})</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No books available</p>
+        )}
+      </div> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/newproject/api/views.py
+++ server/newproject/api/views.py
@@ -4,8 +4,20 @@ from rest_framework import status
 from .models import book
 from .serializer import BookSerializer
 
+# get is for reading data
 @api_view(['GET'])
 def get_books(request):
     books = book.objects.all()
     serialized_data = BookSerializer(books, many=True).data
     return Response(serialized_data)
+
+
+# post is for altering or creating data
+@api_view(['POST'])
+def create_book(request):
+    data =  request.data
+    serializer =  BookSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
